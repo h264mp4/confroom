8dc5ec6015e61e41aa34474c71f4e49a629f9f67
roshask <from-p@myp.com>
2014-10-27 20:33:26 +0800

    addRoom part almost done

--- Handler/AddRoom.hs
+++ Handler/AddRoom.hs
@@ -25,39 +25,27 @@ postAddRoomR = do
     ((result, formWidget), formEnctype) <- runFormPost addRoomForm
     let handlerName = "postAddRoomR" :: Text
     case result of
-        FormFailure errMsg -> do
-            liftIO $ print "Get Form Data Faile"
-            defaultLayout $ do
-                backWidget
-        --        toWidget [hamlet|
-        --                    <div class="row">
-        --                        <div class="col-md-12">
-        --                            <h3> 无效的会议室信息: @{errMsg}, 请重新输入.
-        --                 |]
+        FormFailure errMsg -> defaultLayout $ backWidget ("无效的会议室信息, 请重新输入." :: String)
 
         FormSuccess formInfo -> do
             mayRoomId <- runDB $ addNewRoom formInfo
             case mayRoomId of
-                 Nothing -> defaultLayout $ do
-                     backWidget         
-                     toWidget [hamlet| <h3> 会议室信息已存在，请重新输入.|]
+                 Nothing -> defaultLayout $ backWidget  ("会议室信息已存在，请重新输入" :: String)
 
                  Just roomId -> do
                      liftIO $ print ("Add new room done: " ++ show (fromJust mayRoomId))
                      liftIO $ print formInfo
-                     defaultLayout $ do
-                         toWidget [hamlet|
-                             <div class="row">
-                                 <div class="col-md-12">
-                                     <h3> 会议室信息已保存
-                                 <div class="col-md-12"> 
-                                     <p> #{show formInfo} 
-                           |]
+                     defaultLayout $ backWidget (toHtmlRoomInfo formInfo)
+
     where
-    backWidget = toWidget [hamlet|
+    backWidget info = toWidget [hamlet|
                             <div class="row">
                                 <div class="col-md-12">
                                     <input type=button value="返回" class="btn btn-primary" onClick="location.href='@{AddRoomR}'">
+                                <div class="col-md-12">
+                                     <h3> 会议室信息已保存
+                                <div class="col-md-12"> 
+                                    <p> #{preEscapedToMarkup info}                     
 |]
 
 simpleFormLayoutForAddRoom = BootstrapHorizontalForm
@@ -80,3 +68,15 @@ addRoomForm = renderBootstrap3 simpleFormLayoutForAddRoom $ Room
     where
     authLevel :: [(Text, Level)]
     authLevel = [("普通", AuthNormal), ("领导", AuthAdvance),("管理员", AuthAdmin)]
+
+
+------------------------------------------------------------------------------------------
+---- other helpers
+
+toHtmlRoomInfo :: Room -> String
+toHtmlRoomInfo roomInfo = (
+    "会议室编号: " ++ (show $ roomNumber roomInfo) ++ "<br />  " ++
+    "预订权限: " ++ (show $ roomLevel roomInfo) ++ "<br />  " ++
+    "即时启用: " ++ (show $ roomAvailable roomInfo) ++ "<br />  " ++
+    "会议室有效期至: " ++ (show $ roomValidTime roomInfo) ++ "<br />  " ++
+    "会议室添加日期: " ++ (show $ roomFirstAdd roomInfo) ++ "<br />")
--- Handler/MiscTypes.hs
+++ Handler/MiscTypes.hs
@@ -5,8 +5,13 @@ import Database.Persist.TH
 import Data.Time
 
 data Level = AuthNormal | AuthAdvance |AuthAdmin 
-    deriving (Show, Read, Eq, Ord)
+    deriving (Read, Eq, Ord)
 derivePersistField "Level"
+
+instance Show Level where
+    show AuthNormal  = "普通 "
+    show AuthAdvance = "领导"
+    show AuthAdmin   = "管理员"
 
 -- start and end of a room's booking 
 data Timespan = Timespan TimeOfDay TimeOfDay
